80da66bcf908c7cd8aaa9654bd070283160b0dbb
Rickboot <rickallen@gmail.com>
2023-08-16 20:58:55 -0700

    add package.json override to use current mongoose versions for  mongoose-currency

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.get('/transactions', async (_req, res) => {
   try {
-    const transactions = await Transaction.find();
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdAt: -1 });
     res.status(200).json(transactions);
   } catch (error) {
     res.status(404);
